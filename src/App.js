e93fbf06f8e030e9edb619900547e3ff572f4c73
ClayData <claytonortiz96@gmail.com>
2020-03-25 20:35:59 -0500

    added sign in page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from './components/header/header.component';
 
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shops' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
