1568c140298e308e93fb45430657ad404670d56c
ClayData <claytonortiz96@gmail.com>
2020-03-27 10:58:30 -0500

    added google authentication

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    } 
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+    })
+  }
+ 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
   return (
     <div >
       <Header />
@@ -22,5 +44,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
